061f8936745cd45c04b544e51f1c7e24b63d0601
Alok Singh <alokbeniwal@gmail.com>
2017-10-05 13:19:23 -0700

    Cleanup code

--- shorten_path_for_notational_fzf.py
+++ shorten_path_for_notational_fzf.py
@@ -2,14 +2,11 @@
 # encoding: utf-8
 
 import os
-import pathlib
 import sys
-
-# Type alias.
-Path = str
+from pathlib import PurePath as P
 
 
-def expand_path(path: Path) -> Path:
+def expand_path(path: str) -> str:
     ''' Expand tilde and return absolute path. '''
 
     return os.path.abspath(os.path.expanduser(path))
@@ -20,15 +17,15 @@ REPLACEMENTS = ('', os.pardir, '~')
 old_paths = [expand_path(replacement) for replacement in REPLACEMENTS]
 
 
-def prettyprint_path(path: Path, old_path: Path, replacement: Path) -> Path:
+def prettyprint_path(path: str, old_path: str, replacement: str) -> str:
     # Pretty print the path prefix
     path = path.replace(old_path, replacement, 1)
     # Truncate the rest of the path to a single character.
-    short_path = os.path.join(replacement, * [x[0] for x in pathlib.PurePath(path).parts[1:]])
+    short_path = os.path.join(replacement, * [x[0] for x in P(path).parts[1:]])
     return short_path
 
 
-def shorten(path: Path) -> Path:
+def shorten(path: str) -> str:
     # We don't want to shorten the filename, just its parent directory, so we
     # `split()` and just shorten `path`.
     path, filename = os.path.split(path)
@@ -43,14 +40,13 @@ def shorten(path: Path) -> Path:
 
     # If no replacement was found, shorten the entire path.
     else:
-        short_path = os.path.join(* [x[0] for x in pathlib.PurePath(path).parts])
+        short_path = os.path.join(* [x[0] for x in P(path).parts])
 
-    # This list will always have len 2, so we can unpack it.
     return os.path.join(short_path, filename)
 
 
-def process_line(line) -> None:
-    # Expected format is colon separated (name:linenum:contents)
+def process_line(line: str) -> None:
+    # Expected format is colon separated `name:linenum:contents`
     filename, linenum, contents = line.split(sep=':', maxsplit=2)
 
     # Normalize path for further processing.
@@ -59,11 +55,10 @@ def process_line(line) -> None:
     # Drop trailing newline.
     contents = contents.rstrip()
 
-    # format is: long form, short form, rest of line. This is so vim can process it.
+    # Format is: long form, short form, rest of line. This is so Vim can process it.
     formatted_line = ':'.join([filename, linenum] + [shorten(filename), linenum] + [contents])
 
-    # We print the long and short forms, and one is picked in the Vim script
-    # that uses this.
+    # We print the long and short forms, and one form is picked in the Vim script that uses this.
     print(formatted_line)
 
 
