4ad3366d09b68d7a1aa7ad1db585b81d40d5c6f9
yory8 <39745367+yory8@users.noreply.github.com>
2020-05-01 18:26:59 +0200

    fix: keep selected line visible in preview
    This fixes bug #58

--- print_lines.py
+++ print_lines.py
@@ -2,21 +2,11 @@
 # -*- coding: utf-8 -*-
 """Colorize the current line in the preview window in bold red."""
 
-import os
 import os.path as path
-import shutil
 import sys
 
 line = int(sys.argv[1])
 file = sys.argv[2]
-
-# Use a large default for width since the extra doesn't get displayed anyway
-width = max(1, shutil.get_terminal_size().lines // 2)
-end_width = 200
-
-# file numbers start at 1
-beginning = max(1, line - width)
-end = line + end_width
 
 # ANSI escape sequences for coloring matched line
 RED = "\033[1;31m"
@@ -24,10 +14,14 @@ RESET = "\033[0;0m"
 BOLD = "\033[;1m"
 
 if __name__ == "__main__":
+    is_sel = False
     with open(path.normpath(file)) as f:
         for linenum, line_content in enumerate(f, start=1):
-            if beginning <= linenum <= end:
-                if linenum == line:
-                    print(BOLD + RED + line_content.rstrip() + RESET)
-                else:
+            if linenum == line:
+                print(BOLD + RED + line_content.rstrip() + RESET)
+                is_sel = True
+            elif is_sel or (line - linenum <= 3):
+                    # TODO: rather than 3 we want something like
+                    # "(half the preview window) - 1",
+                    # so the current line is always centered
                     print(line_content.rstrip())
